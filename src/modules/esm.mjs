c7db72206ee563987c6e0a76ebaa8b8a5d504232
u_m1h6d <DTsarev@alfabank.ru>
2022-12-05 17:00:02 +0300

    refactor: refactor file from .cjs to .mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,19 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import url from 'url';
+import './files/c.js';
 
 const random = Math.random();
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', { assert: { type: 'json'}});
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import('./files/b.json', { assert: { type: 'json'}});;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +36,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
-
