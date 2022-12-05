9c568a0dcb814634bb42da848eab57bde0e96b8f
u_m1h6d <DTsarev@alfabank.ru>
2022-12-05 21:51:23 +0300

    feat: zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import url from 'url';
+import path from 'path';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const fileToCompressPath = path.resolve(__dirname, 'files/fileToCompress.txt');
+    const compressedFilePath = path.resolve(__dirname, 'files/archive.gz');
+    const readStream = createReadStream(compressedFilePath);
+
+    readStream
+        .pipe(createUnzip())
+        .pipe(createWriteStream(fileToCompressPath))
+        .on('finish', () => {
+            console.log('file decompressed!');
+        });
 };
 
 await decompress();
