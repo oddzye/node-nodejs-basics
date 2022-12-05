c3cbcabf669f80c749100fd17d2d35879ce33c94
u_m1h6d <DTsarev@alfabank.ru>
2022-12-05 18:04:33 +0300

    feat: add calculate hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'crypto';
+import path from 'path';
+import url from 'url';
+import { readFile } from 'fs/promises';
+
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = path.resolve(__dirname, 'files/fileToCalculateHashFor.txt');
+    const fileContent = await readFile(filePath, { encoding: 'utf-8'});
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+
+    console.log(hash);
 };
 
 await calculateHash();
